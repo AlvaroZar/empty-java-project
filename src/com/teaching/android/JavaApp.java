ab1e96e1a35120b05859f0f3344f90734ba57ad2
LAPTOP-CMHOFM9J\Zar <alvarozarque@gmail.com>
2018-04-18 12:43:39 +0200

    4

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -7,44 +7,14 @@ import java.util.Scanner;
  * Empty Java App for teaching purposes.
  */
 public class JavaApp {
-    
+
     public static void main(String[] args) {
 
-        System.out.println("Escribe un numero");
-        Scanner keyboard = new Scanner(System.in);
-        int valor = keyboard.nextInt();
-        Random generator = new Random();
-        int random = generator.nextInt(10) + 1;
+      for (int i=0;i<=10;i++){
 
-        if (valor == random){
-            System.out.println("Es Correcto");
-            System.exit(0);
-        }
-        else if (valor > random) {
-            System.out.println("Es mayor");
-        }
-        else  {
-            System.out.println("Es menor");
+              System.out.println(new Random().nextInt(10)+1);
+
+         }
         }
 
-        valor = keyboard.nextInt();
-        if (valor == random){
-            System.out.println("Es Correcto");
-            System.exit(0);
-        }
-        else if (valor > random) {
-            System.out.println("Es mayor");
-        }
-        else  {
-            System.out.println("Es menor");
-        }
-
-        valor = keyboard.nextInt();
-        if (valor == random){
-            System.out.println("Es Correcto");
-            System.exit(0);
-        }
-        System.out.println("Has perdido el numero correcto es " + random);
     }
-
-}
