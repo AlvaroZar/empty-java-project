fc2da35352c08bd0a5af08f3f2fcfd7aefac2473
LAPTOP-CMHOFM9J\Zar <alvarozarque@gmail.com>
2018-04-18 10:54:18 +0200

    3

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -1,23 +1,50 @@
 package com.teaching.android;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Empty Java App for teaching purposes.
  */
 public class JavaApp {
-
+    
     public static void main(String[] args) {
-        System.out.println("Escribe un numero");
-        Scanner scanner = new Scanner(System.in);
-        int valor = scanner.nextInt();
 
-        if(valor % 2 == 0) {
-            System.out.println("Es par");
+        System.out.println("Escribe un numero");
+        Scanner keyboard = new Scanner(System.in);
+        int valor = keyboard.nextInt();
+        Random generator = new Random();
+        int random = generator.nextInt(10) + 1;
+
+        if (valor == random){
+            System.out.println("Es Correcto");
+            System.exit(0);
         }
-        else {
-            System.out.println("Es impar");
+        else if (valor > random) {
+            System.out.println("Es mayor");
         }
+        else  {
+            System.out.println("Es menor");
+        }
+
+        valor = keyboard.nextInt();
+        if (valor == random){
+            System.out.println("Es Correcto");
+            System.exit(0);
+        }
+        else if (valor > random) {
+            System.out.println("Es mayor");
+        }
+        else  {
+            System.out.println("Es menor");
+        }
+
+        valor = keyboard.nextInt();
+        if (valor == random){
+            System.out.println("Es Correcto");
+            System.exit(0);
+        }
+        System.out.println("Has perdido el numero correcto es " + random);
     }
 
 }
